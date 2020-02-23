95c021e2b4e7c853045d57e5a20e991fcf0122f9
wilsder castillo chen <dante27000@hotmail.com>
2020-02-23 10:54:46 -0600

    Beta de el fragmento de noticias
    adicion de un webview para poder ver las noticias de gitce

--- app/src/main/java/com/example/gitce/NoticiasFragment.java
+++ app/src/main/java/com/example/gitce/NoticiasFragment.java
@@ -17,10 +17,14 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.gitce.R;
 
 public class NoticiasFragment extends Fragment {
-
+    WebView noti;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_noticias, container, false);
+        noti=(WebView)root.findViewById(R.id.noticias);
+        noti.getSettings().setJavaScriptEnabled(true);
+        noti.setWebViewClient(new WebViewClient());
+        noti.loadUrl("http://www.gitce.utp.ac.pa/vernot");
         return root;
 
     }
